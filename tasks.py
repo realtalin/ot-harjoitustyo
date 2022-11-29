904d1580f3ef3a1967fe2c1d44e597b2276b7d19
Sampo <57003465+realtalin@users.noreply.github.com>
2022-11-29 20:47:58 +0200

    Add invoke task for pylint

--- tasks.py
+++ tasks.py
@@ -12,3 +12,7 @@ def test(ctx):
 def coverage_report(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
     ctx.run("coverage html", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
